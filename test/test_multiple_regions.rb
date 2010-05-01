f62d0780f55575724369585e25a6e864cd87cfed
Rowan Crawford <rowanc@smx.co.nz>
2010-05-01 23:11:58 +1200

    proper test for multi-region requiring

--- test/test_multiple_regions.rb
+++ test/test_multiple_regions.rb
@@ -1,6 +1,4 @@
 require File.dirname(__FILE__) + '/test_helper'
-require 'holidays/gb'
-require 'holidays/ie'
 
 class MultipleRegionsTests < Test::Unit::TestCase
   def setup
@@ -10,6 +8,10 @@ class MultipleRegionsTests < Test::Unit::TestCase
   def test_defining_holidays
     h = Holidays.on(Date.civil(2008,12,26), :ie)
     assert_equal 'St. Stephen\'s Day', h[0][:name]
+
+    h = Holidays.on(Date.civil(2008,5,9), :gb_)
+    assert_equal 'Liberation Day', (h[0] || {})[:name]
+
 
     h = Holidays.on(Date.civil(2008,5,9), :je)
     assert_equal 'Liberation Day', h[0][:name]
