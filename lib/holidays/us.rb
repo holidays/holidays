a9cfdc731f8c1fa62a2222b6661979ded3413fdf
Eric Heikes <eheikes@gmail.com>
2013-07-15 11:53:13 -0500

    Fix Father's Day (which also fixes Mother's Day and Armed Forces Day).
    Added tests for North American holidays.

--- lib/holidays/us.rb
+++ lib/holidays/us.rb
@@ -26,7 +26,8 @@ module Holidays
             {:mday => 2, :type => :informal, :name => "Groundhog Day", :regions => [:us, :ca]},
             {:mday => 14, :type => :informal, :name => "Valentine's Day", :regions => [:us, :ca]}],
       5 => [{:wday => 1, :week => -1, :name => "Memorial Day", :regions => [:us]},
-            {:wday => 0, :week => 3, :type => :informal, :name => "Father's Day", :regions => [:us, :ca]}],
+            {:wday => 0, :week => 2, :type => :informal, :name => "Mother's Day", :regions => [:us, :ca]},
+            {:wday => 6, :week => 3, :type => :informal, :name => "Armed Forces Day", :regions => [:us]}],
       7 => [{:mday => 4, :observed => lambda { |date| Holidays.to_weekday_if_weekend(date) }, :observed_id => "to_weekday_if_weekend", :name => "Independence Day", :regions => [:us]}],
       9 => [{:wday => 1, :week => 1, :name => "Labor Day", :regions => [:us]}],
       10 => [{:wday => 1, :week => 2, :name => "Columbus Day", :regions => [:us]},
@@ -36,7 +37,8 @@ module Holidays
       12 => [{:mday => 25, :observed => lambda { |date| Holidays.to_weekday_if_weekend(date) }, :observed_id => "to_weekday_if_weekend", :name => "Christmas Day", :regions => [:us]}],
       3 => [{:mday => 17, :type => :informal, :name => "St. Patrick's Day", :regions => [:us, :ca]}],
       4 => [{:mday => 1, :type => :informal, :name => "April Fool's Day", :regions => [:us, :ca]},
-            {:mday => 22, :type => :informal, :name => "Earth Day", :regions => [:us, :ca]}]
+            {:mday => 22, :type => :informal, :name => "Earth Day", :regions => [:us, :ca]}],
+      6 => [{:wday => 0, :week => 3, :type => :informal, :name => "Father's Day", :regions => [:us, :ca]}]
       }
     end
   end
--- test/defs/test_defs_mx.rb
+++ test/defs/test_defs_mx.rb
@@ -18,5 +18,18 @@ class MxDefinitionTests < Test::Unit::TestCase  # :nodoc:
  Date.civil(2007,12,25) => 'Navidad'}.each do |date, name|
   assert_equal name, (Holidays.on(date, :mx, :informal)[0] || {})[:name]
 end  
+
+{Date.civil(2013,2,2) => 'Groundhog Day',
+ Date.civil(2013,2,14) => 'Valentine\'s Day',
+ Date.civil(2013,3,17) => 'St. Patrick\'s Day',
+ Date.civil(2013,4,1) => 'April Fool\'s Day',
+ Date.civil(2013,4,22) => 'Earth Day',
+ Date.civil(2013,5,12) => 'Mother\'s Day',
+ Date.civil(2013,5,18) => 'Armed Forces Day',
+ Date.civil(2013,6,16) => 'Father\'s Day',
+ Date.civil(2013,10,31) => 'Halloween'}.each do |date, name|
+  assert_equal name, (Holidays.on(date, :us, :informal)[0] || {})[:name]
+end
+
   end
 end
