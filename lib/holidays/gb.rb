70c47036c9914ed65e65eebd37731b79ed2b19e1
Andy Waite <andywaite@notonthehighstreet.com>
2013-08-19 14:47:25 +0100

    Fix New Year's Day observed holiday for GB
    If New Year's day falls on a Saturday or Sunday, the following Monday is
    treated as a substitute holiday.
    
    See https://www.gov.uk/bank-holidays

--- lib/holidays/gb.rb
+++ lib/holidays/gb.rb
@@ -21,7 +21,7 @@ module Holidays
               0 => [{:function => lambda { |year| Holidays.easter(year)-2 }, :function_id => "easter(year)-2", :name => "Good Friday", :regions => [:gb]},
             {:function => lambda { |year| Holidays.easter(year) }, :function_id => "easter(year)", :name => "Easter Sunday", :regions => [:gb]},
             {:function => lambda { |year| Holidays.easter(year)+1 }, :function_id => "easter(year)+1", :name => "Easter Monday", :regions => [:gb_eng, :gb_wls, :gb_eaw, :gb_nir]}],
-      1 => [{:mday => 1, :name => "New Year's Day", :regions => [:gb]},
+      1 => [{:mday => 1, :observed => lambda { |date| Holidays.to_monday_if_weekend(date) }, :observed_id => "to_monday_if_weekend", :name => "New Year's Day", :regions => [:gb]},
             {:mday => 2, :name => "New Year's", :regions => [:gb_sct]}],
       3 => [{:mday => 5, :name => "St. Piran's Day", :regions => [:gb_con]},
             {:mday => 17, :name => "St. Patrick's Day", :regions => [:gb_nir]}],
