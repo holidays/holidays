1728cf32cfa07ab065dbc3ead4475a270bc917a2
Mattias Ekberg <mattias@mattiasekberg.com>
2014-06-24 22:34:40 +0200

    Added informal swedish holidays.

--- test/defs/test_defs_se.rb
+++ test/defs/test_defs_se.rb
@@ -9,7 +9,8 @@ class SeDefinitionTests < Test::Unit::TestCase  # :nodoc:
   def test_se
 {Date.civil(2008,1,1) => 'Nyårsdagen', 
  Date.civil(2008,1,6) => 'Trettondedag jul',
- Date.civil(2008,3,21) => 'Långfredagen', 
+ Date.civil(2008,3,21) => 'Långfredagen',
+ Date.civil(2008,3,22) => 'Påskafton',
  Date.civil(2008,3,23) => 'Påskdagen', 
  Date.civil(2008,3,24) => 'Annandag påsk',
  Date.civil(2008,5,1) => 'Första maj',
@@ -18,14 +19,20 @@ class SeDefinitionTests < Test::Unit::TestCase  # :nodoc:
  Date.civil(2008,6,6) => 'Nationaldagen',
  Date.civil(2005,6,25) => 'Midsommardagen',
  Date.civil(2006,6,24) => 'Midsommardagen',
+ Date.civil(2006,6,23) => 'Midsommarafton',
  Date.civil(2007,6,23) => 'Midsommardagen',
+ Date.civil(2007,6,22) => 'Midsommarafton',
  Date.civil(2008,6,21) => 'Midsommardagen',
+ Date.civil(2008,6,20) => 'Midsommarafton',
  Date.civil(2005,11,5) => 'Alla helgons dag',
  Date.civil(2006,11,4) => 'Alla helgons dag',
  Date.civil(2007,11,3) => 'Alla helgons dag',
  Date.civil(2008,11,1) => 'Alla helgons dag',
+ Date.civil(2008,12,24) => 'Julafton',
  Date.civil(2008,12,25) => 'Juldagen',
- Date.civil(2008,12,26) => 'Annandag jul'}.each do |date, name|
+ Date.civil(2008,12,26) => 'Annandag jul',
+ Date.civil(2008,12,31) => 'Nyårsafton'
+ }.each do |date, name|
   assert_equal name, (Holidays.on(date, :se, :informal)[0] || {})[:name]
 end
   end
