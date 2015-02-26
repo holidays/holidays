9a0ae8fda21152f10c99128ea50e748a673e2099
maximerety <maxime.rety@gmail.com>
2015-02-26 19:49:00 +0100

    Belgian holidays should be written in french instead of english

--- test/defs/test_defs_be.rb
+++ test/defs/test_defs_be.rb
@@ -7,18 +7,18 @@ require File.expand_path(File.dirname(__FILE__)) + '/../test_helper'
 class BeDefinitionTests < Test::Unit::TestCase  # :nodoc:
 
   def test_be
-{Date.civil(2007,1,1) => 'New Year\'s Day', 
- Date.civil(2007,4,8) => 'Easter',
- Date.civil(2007,4,9) => 'Easter Monday',
- Date.civil(2007,5,1) => 'Labour Day',
+{Date.civil(2007,1,1) => 'Jour de l\'an',
+ Date.civil(2007,4,8) => 'Pâques',
+ Date.civil(2007,4,9) => 'Lundi de Pâques',
+ Date.civil(2007,5,1) => 'Fête du travail',
  Date.civil(2007,5,17) => 'Ascension',
- Date.civil(2007,5,27) => 'Pentecost',
- Date.civil(2007,5,28) => 'Pentecost Monday',
- Date.civil(2007,7,21) => 'National holiday',
- Date.civil(2007,8,15) => 'Assumption of Mary',
- Date.civil(2007,11,1) => 'All Saints',
- Date.civil(2007,11,11) => 'Armistice Day',
- Date.civil(2007,12,25) => 'Christmas'}.each do |date, name|
+ Date.civil(2007,5,27) => 'Pentecôte',
+ Date.civil(2007,5,28) => 'Lundi de Pentecôte',
+ Date.civil(2007,7,21) => 'Fête nationale',
+ Date.civil(2007,8,15) => 'Assomption',
+ Date.civil(2007,11,1) => 'Toussaint',
+ Date.civil(2007,11,11) => 'Armistice 1918',
+ Date.civil(2007,12,25) => 'Noël'}.each do |date, name|
   assert_equal name, (Holidays.on(date, :be, :informal)[0] || {})[:name]
 end
   end
