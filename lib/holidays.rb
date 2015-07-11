4dfa6ec7a96b8be0cc6dcb2a65b622ea9ee42cdf
Phil Trimble <ptrimble@sittercity.com>
2015-07-11 03:21:55 +0000

    Rewrite: switch style of main holidays module class methods to make real private methods

--- lib/holidays.rb
+++ lib/holidays.rb
@@ -55,234 +55,236 @@ module Holidays
   DEFINITIONS_PATH = 'generated_definitions'
   FULL_DEFINITIONS_PATH = File.expand_path(File.dirname(__FILE__) + "/#{DEFINITIONS_PATH}")
 
-  # Get all holidays on a given date.
-  #
-  # [<tt>date</tt>]     A Date object.
-  # [<tt>:options</tt>] One or more region symbols, <tt>:informal</tt> and/or <tt>:observed</tt>.
-  #
-  # Returns an array of hashes or nil. See Holidays#between for the output
-  # format.
-  #
-  # Also available via Date#holidays.
-  def self.on(date, *options)
-    self.between(date, date, options)
-  end
-
-  # Does the given work-week have any holidays?
-  #
-  # [<tt>date</tt>]   A Date object.
-  # [<tt>:options</tt>] One or more region symbols, and/or <tt>:informal</tt>. Automatically includes <tt>:observed</tt>. If you don't want this, pass <tt>:no_observed</tt>
-  #
-  # The given Date can be any day of the week.
-  # Returns true if any holidays fall on Monday - Friday of the given week.
-  def self.full_week?(date, *options)
-    days_to_monday = date.wday - 1
-    days_to_friday = 5 - date.wday
-    start_date = date - days_to_monday
-    end_date = date + days_to_friday
-    options += [:observed] unless options.include?(:no_observed)
-    options.delete(:no_observed)
-    self.between(start_date, end_date, options).empty?
-  end
-
-  # Get all holidays occuring between two dates, inclusively.
-  #
-  # Returns an array of hashes or nil.
-  #
-  # Each holiday is returned as a hash with the following fields:
-  # [<tt>start_date</tt>]  Ruby Date object.
-  # [<tt>end_date</tt>]    Ruby Date object.
-  # [<tt>options</tt>]     One or more region symbols, <tt>:informal</tt> and/or <tt>:observed</tt>.
-  #
-  # ==== Example
-  #   from = Date.civil(2008,7,1)
-  #   to   = Date.civil(2008,7,31)
-  #
-  #   Holidays.between(from, to, :ca, :us)
-  #   => [{:name => 'Canada Day', :regions => [:ca]...}
-  #       {:name => 'Independence Day'', :regions => [:us], ...}]
-  def self.between(start_date, end_date, *options)
-    # remove the timezone
-    start_date = start_date.new_offset(0) + start_date.offset if start_date.respond_to?(:new_offset)
-    end_date = end_date.new_offset(0) + end_date.offset if end_date.respond_to?(:new_offset)
-
-    # get simple dates
-    start_date, end_date = get_date(start_date), get_date(end_date)
-
-    if cached_holidays = DefinitionFactory.cache_repository.find(start_date, end_date, options)
-      return cached_holidays
+  class << self
+    # Get all holidays on a given date.
+    #
+    # [<tt>date</tt>]     A Date object.
+    # [<tt>:options</tt>] One or more region symbols, <tt>:informal</tt> and/or <tt>:observed</tt>.
+    #
+    # Returns an array of hashes or nil. See Holidays#between for the output
+    # format.
+    #
+    # Also available via Date#holidays.
+    def on(date, *options)
+      between(date, date, options)
     end
 
-    regions, observed, informal = OptionFactory.parse_options.call(options)
-    holidays = []
-
-    dates = {}
-    (start_date..end_date).each do |date|
-      # Always include month '0' for variable-month holidays
-      dates[date.year] = [0] unless dates[date.year]
-      # TODO: test this, maybe should push then flatten
-      dates[date.year] << date.month unless dates[date.year].include?(date.month)
+    # Does the given work-week have any holidays?
+    #
+    # [<tt>date</tt>]   A Date object.
+    # [<tt>:options</tt>] One or more region symbols, and/or <tt>:informal</tt>. Automatically includes <tt>:observed</tt>. If you don't want this, pass <tt>:no_observed</tt>
+    #
+    # The given Date can be any day of the week.
+    # Returns true if any holidays fall on Monday - Friday of the given week.
+    def full_week?(date, *options)
+      days_to_monday = date.wday - 1
+      days_to_friday = 5 - date.wday
+      start_date = date - days_to_monday
+      end_date = date + days_to_friday
+      options += [:observed] unless options.include?(:no_observed)
+      options.delete(:no_observed)
+      between(start_date, end_date, options).empty?
     end
 
-    dates.each do |year, months|
-      months.each do |month|
-        next unless hbm = DefinitionFactory.holidays_by_month_repository.find_by_month(month)
+    # Get all holidays occuring between two dates, inclusively.
+    #
+    # Returns an array of hashes or nil.
+    #
+    # Each holiday is returned as a hash with the following fields:
+    # [<tt>start_date</tt>]  Ruby Date object.
+    # [<tt>end_date</tt>]    Ruby Date object.
+    # [<tt>options</tt>]     One or more region symbols, <tt>:informal</tt> and/or <tt>:observed</tt>.
+    #
+    # ==== Example
+    #   from = Date.civil(2008,7,1)
+    #   to   = Date.civil(2008,7,31)
+    #
+    #   Holidays.between(from, to, :ca, :us)
+    #   => [{:name => 'Canada Day', :regions => [:ca]...}
+    #       {:name => 'Independence Day'', :regions => [:us], ...}]
+    def between(start_date, end_date, *options)
+      # remove the timezone
+      start_date = start_date.new_offset(0) + start_date.offset if start_date.respond_to?(:new_offset)
+      end_date = end_date.new_offset(0) + end_date.offset if end_date.respond_to?(:new_offset)
 
-        hbm.each do |h|
-          next unless in_region?(regions, h[:regions])
+      # get simple dates
+      start_date, end_date = get_date(start_date), get_date(end_date)
 
-          # Skip informal holidays unless they have been requested
-          next if h[:type] == :informal and not informal
+      if cached_holidays = DefinitionFactory.cache_repository.find(start_date, end_date, options)
+        return cached_holidays
+      end
 
-          if h[:function]
-            # Holiday definition requires a calculation
-            result = call_proc(h[:function], year)
+      regions, observed, informal = OptionFactory.parse_options.call(options)
+      holidays = []
 
-            # Procs may return either Date or an integer representing mday
-            if result.kind_of?(Date)
-              month = result.month
-              mday = result.mday
+      dates = {}
+      (start_date..end_date).each do |date|
+        # Always include month '0' for variable-month holidays
+        dates[date.year] = [0] unless dates[date.year]
+        # TODO: test this, maybe should push then flatten
+        dates[date.year] << date.month unless dates[date.year].include?(date.month)
+      end
+
+      dates.each do |year, months|
+        months.each do |month|
+          next unless hbm = DefinitionFactory.holidays_by_month_repository.find_by_month(month)
+
+          hbm.each do |h|
+            next unless in_region?(regions, h[:regions])
+
+            # Skip informal holidays unless they have been requested
+            next if h[:type] == :informal and not informal
+
+            if h[:function]
+              # Holiday definition requires a calculation
+              result = call_proc(h[:function], year)
+
+              # Procs may return either Date or an integer representing mday
+              if result.kind_of?(Date)
+                month = result.month
+                mday = result.mday
+              else
+                mday = result
+              end
             else
-              mday = result
+              # Calculate the mday
+              mday = h[:mday] || DateCalculator.day_of_month(year, month, h[:week], h[:wday])
             end
-          else
-            # Calculate the mday
-            mday = h[:mday] || DateCalculator.day_of_month(year, month, h[:week], h[:wday])
+
+            # Silently skip bad mdays
+            begin
+              date = Date.civil(year, month, mday)
+            rescue; next; end
+
+            # If the :observed option is set, calculate the date when the holiday
+            # is observed.
+            if observed and h[:observed]
+              date = call_proc(h[:observed], date)
+            end
+
+            if date.between?(start_date, end_date)
+              holidays << {:date => date, :name => h[:name], :regions => h[:regions]}
+            end
+
           end
-
-          # Silently skip bad mdays
-          begin
-            date = Date.civil(year, month, mday)
-          rescue; next; end
-
-          # If the :observed option is set, calculate the date when the holiday
-          # is observed.
-          if observed and h[:observed]
-            date = call_proc(h[:observed], date)
-          end
-
-          if date.between?(start_date, end_date)
-            holidays << {:date => date, :name => h[:name], :regions => h[:regions]}
-          end
-
         end
+      end
+
+      holidays.sort{|a, b| a[:date] <=> b[:date] }
+    end
+
+    # Allows a developer to explicitly calculate and cache holidays within a given period
+    def cache_between(start_date, end_date, *options)
+      start_date, end_date = get_date(start_date), get_date(end_date)
+      cache_data = between(start_date, end_date, *options)
+
+      DefinitionFactory.cache_repository.cache_between(start_date, end_date, cache_data, options)
+    end
+
+    #TODO This should not be publicly available. I need to restructure the public
+    #     API for this class to something more sensible.
+    def merge_defs(regions, holidays) # :nodoc:
+      DefinitionFactory.merger.call(regions, holidays)
+    end
+
+    def easter(year)
+      DateCalculator.calculate_easter_for(year)
+    end
+
+    def orthodox_easter(year)
+      DateCalculator.calculate_orthodox_easter_for(year)
+    end
+
+    def to_monday_if_sunday(date)
+      DateCalculator.to_monday_if_sunday(date)
+    end
+
+    def to_monday_if_weekend(date)
+      DateCalculator.to_monday_if_weekend(date)
+    end
+
+    # Move Boxing Day if it falls on a weekend, leaving room for Christmas.
+    # Used as an observed function.
+    def to_weekday_if_boxing_weekend(date)
+      DateCalculator.to_weekday_if_boxing_weekend(date)
+    end
+
+    # Call to_weekday_if_boxing_weekend but first get date based on year
+    # Used as a callback function.
+    def to_weekday_if_boxing_weekend_from_year(year)
+      to_weekday_if_boxing_weekend(Date.civil(year, 12, 26))
+    end
+
+    # Move date to Monday if it occurs on a Sunday or to Friday if it occurs on a
+    # Saturday.
+    # Used as a callback function.
+    def to_weekday_if_weekend(date)
+      DateCalculator.to_weekday_if_weekend(date)
+    end
+
+    # Returns an array of symbols all the available holiday definitions.
+    #
+    # Optional `full_path` param is used internally for loading all the definitions.
+    def available(full_path = false)
+      paths = Dir.glob(FULL_DEFINITIONS_PATH + '/*.rb')
+      full_path ? paths : paths.collect { |path| path.match(/([a-z_-]+)\.rb/i)[1].to_sym }
+    end
+
+    # Returns an array of symbols of all the available holiday regions.
+    def regions
+      DefinitionFactory.regions_repository.all
+    end
+
+    # Load all available holiday definitions
+    def load_all
+      available(true).each { |path| require path }
+    end
+
+    # Parses provided holiday definition file(s) and loads them so that they are immediately available.
+    def load_custom(*files)
+      regions, rules_by_month, custom_methods, tests = DefinitionFactory.file_parser.parse_definition_files(files)
+      merge_defs(regions, rules_by_month)
+    end
+
+    # Parses provided holiday definition file(s) and returns strings containing the generated module and test source
+    def parse_definition_files_and_return_source(module_name, *files)
+      regions, rules_by_month, custom_methods, tests = DefinitionFactory.file_parser.parse_definition_files(files)
+      module_src, test_src = DefinitionFactory.source_generator.generate_definition_source(module_name, files, regions, rules_by_month, custom_methods, tests)
+
+      return module_src, test_src
+    end
+
+    private
+
+    def get_date(date)
+      if date.respond_to?(:to_date)
+        date.to_date
+      else
+        Date.civil(date.year, date.mon, date.mday)
       end
     end
 
-    holidays.sort{|a, b| a[:date] <=> b[:date] }
-  end
+    # Check sub regions.
+    #
+    # When request :any, all holidays should be returned.
+    # When requesting :ca_bc, holidays in :ca or :ca_bc should be returned.
+    # When requesting :ca, holidays in :ca but not its subregions should be returned.
+    def in_region?(requested, available) # :nodoc:
+      return true if requested.include?(:any)
 
-  # Allows a developer to explicitly calculate and cache holidays within a given period
-  def self.cache_between(start_date, end_date, *options)
-    start_date, end_date = get_date(start_date), get_date(end_date)
-    cache_data = between(start_date, end_date, *options)
+      # When an underscore is encountered, derive the parent regions
+      # symbol and include both in the requested array.
+      requested = requested.collect do |r|
+        r.to_s =~ /_/ ? [r, r.to_s.gsub(/_[\w]*$/, '').to_sym] : r
+      end
 
-    DefinitionFactory.cache_repository.cache_between(start_date, end_date, cache_data, options)
-  end
+      requested = requested.flatten.uniq
 
-  #TODO This should not be publicly available. I need to restructure the public
-  #     API for this class to something more sensible.
-  def self.merge_defs(regions, holidays) # :nodoc:
-    DefinitionFactory.merger.call(regions, holidays)
-  end
-
-  def self.easter(year)
-    DateCalculator.calculate_easter_for(year)
-  end
-
-  def self.orthodox_easter(year)
-    DateCalculator.calculate_orthodox_easter_for(year)
-  end
-
-  def self.to_monday_if_sunday(date)
-    DateCalculator.to_monday_if_sunday(date)
-  end
-
-  def self.to_monday_if_weekend(date)
-    DateCalculator.to_monday_if_weekend(date)
-  end
-
-  # Move Boxing Day if it falls on a weekend, leaving room for Christmas.
-  # Used as an observed function.
-  def self.to_weekday_if_boxing_weekend(date)
-    DateCalculator.to_weekday_if_boxing_weekend(date)
-  end
-
-  # Call to_weekday_if_boxing_weekend but first get date based on year
-  # Used as a callback function.
-  def self.to_weekday_if_boxing_weekend_from_year(year)
-    to_weekday_if_boxing_weekend(Date.civil(year, 12, 26))
-  end
-
-  # Move date to Monday if it occurs on a Sunday or to Friday if it occurs on a
-  # Saturday.
-  # Used as a callback function.
-  def self.to_weekday_if_weekend(date)
-    DateCalculator.to_weekday_if_weekend(date)
-  end
-
-  # Returns an array of symbols all the available holiday definitions.
-  #
-  # Optional `full_path` param is used internally for loading all the definitions.
-  def self.available(full_path = false)
-    paths = Dir.glob(FULL_DEFINITIONS_PATH + '/*.rb')
-    full_path ? paths : paths.collect { |path| path.match(/([a-z_-]+)\.rb/i)[1].to_sym }
-  end
-
-  # Returns an array of symbols of all the available holiday regions.
-  def self.regions
-    DefinitionFactory.regions_repository.all
-  end
-
-  # Load all available holiday definitions
-  def self.load_all
-    self.available(true).each { |path| require path }
-  end
-
-  # Parses provided holiday definition file(s) and loads them so that they are immediately available.
-  def self.load_custom(*files)
-    regions, rules_by_month, custom_methods, tests = DefinitionFactory.file_parser.parse_definition_files(files)
-    merge_defs(regions, rules_by_month)
-  end
-
-  # Parses provided holiday definition file(s) and returns strings containing the generated module and test source
-  def self.parse_definition_files_and_return_source(module_name, *files)
-    regions, rules_by_month, custom_methods, tests = DefinitionFactory.file_parser.parse_definition_files(files)
-    module_src, test_src = DefinitionFactory.source_generator.generate_definition_source(module_name, files, regions, rules_by_month, custom_methods, tests)
-
-    return module_src, test_src
-  end
-
-private
-
-  def self.get_date(date)
-    if date.respond_to?(:to_date)
-      date.to_date
-    else
-      Date.civil(date.year, date.mon, date.mday)
-    end
-  end
-
-  # Check sub regions.
-  #
-  # When request :any, all holidays should be returned.
-  # When requesting :ca_bc, holidays in :ca or :ca_bc should be returned.
-  # When requesting :ca, holidays in :ca but not its subregions should be returned.
-  def self.in_region?(requested, available) # :nodoc:
-    return true if requested.include?(:any)
-
-    # When an underscore is encountered, derive the parent regions
-    # symbol and include both in the requested array.
-    requested = requested.collect do |r|
-      r.to_s =~ /_/ ? [r, r.to_s.gsub(/_[\w]*$/, '').to_sym] : r
+      available.any? { |avail| requested.include?(avail) }
     end
 
-    requested = requested.flatten.uniq
-
-    available.any? { |avail| requested.include?(avail) }
-  end
-
-  def self.call_proc(function, year) # :nodoc:
-    DefinitionFactory.proc_cache_repository.lookup(function, year)
+    def call_proc(function, year) # :nodoc:
+      DefinitionFactory.proc_cache_repository.lookup(function, year)
+    end
   end
 end
