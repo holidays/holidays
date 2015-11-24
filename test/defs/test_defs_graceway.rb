9e526b80625a72dc30fc9dfb6dbfe7a14e7c6dce
Brent Odell <brent@rtcreativegroup.com>
2015-11-24 12:44:45 -0600

    Clean up excess whitespace & add apostrophes
    - Add apostrophes as needed to Mother's Day, Father's Day, etc

--- test/defs/test_defs_graceway.rb
+++ test/defs/test_defs_graceway.rb
@@ -7,7 +7,7 @@ require File.expand_path(File.dirname(__FILE__)) + '/../test_helper'
 class GracewayDefinitionTests < Test::Unit::TestCase  # :nodoc:
 
   def test_graceway
-{Date.civil(2014,1,1)   => 'New Years', 
+{Date.civil(2014,1,1)   => 'New Years',
  Date.civil(2014,1,19)  => 'Sanctity of Life Week',
  Date.civil(2014,1,20)  => 'Martin Luther King Jr',
  Date.civil(2014,2,14)  => "Valentine\'s Day",
@@ -18,17 +18,17 @@ class GracewayDefinitionTests < Test::Unit::TestCase  # :nodoc:
  Date.civil(2016,4,22)  => 'Passover',
  Date.civil(2014,4,20)  => 'Easter Sunday',
  Date.civil(2014,5,1)   => 'National Day of Prayer',
- Date.civil(2014,5,11)  => 'Mothers Day',
+ Date.civil(2014,5,11)  => "Mother\'s Day",
  Date.civil(2015,5,24)  => 'Pentecost',
  Date.civil(2014,5,26)  => 'Memorial Day',
  Date.civil(2015,5,31)  => 'Trinity Sunday',
- Date.civil(2015,6,21)  => 'Fathers Day',
+ Date.civil(2015,6,21)  => "Father\'s Day",
  Date.civil(2014,7,4)   => 'Independence Day',
  Date.civil(2014,9,1)   => 'Labor Day',
  Date.civil(2014,9,24)  => 'See You at the Pole',
  Date.civil(2014,10,12) => 'Pastor Appreciation',
  Date.civil(2014,11,2)  => 'Daylight Savings Ends',
- Date.civil(2014,11,11) => 'Veterans Day',
+ Date.civil(2014,11,11) => "Veteran\'s Day",
  Date.civil(2014,11,27) => 'Thanksgiving',
  Date.civil(2015,11,29) => 'Advent',
  Date.civil(2014,12,25) => 'Christmas Day'}.each do |date, name|
