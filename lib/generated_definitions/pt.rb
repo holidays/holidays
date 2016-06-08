dc5aab48b596920ae8af79e709ff8ac5e3c96856
Rui Gonçalves <ruippeixotog@gmail.com>
2016-06-09 00:58:35 +0100

    Update Portuguese holidays

--- lib/generated_definitions/pt.rb
+++ lib/generated_definitions/pt.rb
@@ -20,13 +20,17 @@ module Holidays
       {
               0 => [{:function => "easter(year)", :function_arguments => [:year], :function_modifier => -47, :type => :informal, :name => "Carnaval", :regions => [:pt]},
             {:function => "easter(year)", :function_arguments => [:year], :function_modifier => -2, :name => "Sexta-feira Santa", :regions => [:pt]},
-            {:function => "easter(year)", :function_arguments => [:year], :name => "Páscoa", :regions => [:pt]}],
+            {:function => "easter(year)", :function_arguments => [:year], :name => "Páscoa", :regions => [:pt]},
+            {:function => "easter(year)", :function_arguments => [:year], :function_modifier => 60, :name => "Corpo de Deus", :regions => [:pt]}],
       1 => [{:mday => 1, :name => "Ano Novo", :regions => [:pt]}],
       4 => [{:mday => 25, :name => "Dia da Liberdade", :regions => [:pt]}],
       5 => [{:mday => 1, :name => "Dia do Trabalhador", :regions => [:pt]}],
       6 => [{:mday => 10, :name => "Dia de Portugal", :regions => [:pt]}],
       8 => [{:mday => 15, :name => "Assunção de Nossa Senhora", :regions => [:pt]}],
-      12 => [{:mday => 8, :name => "Imaculada Conceição", :regions => [:pt]},
+      10 => [{:mday => 5, :name => "Dia da República", :regions => [:pt]}],
+      11 => [{:mday => 1, :name => "Dia de Todos-os-Santos", :regions => [:pt]}],
+      12 => [{:mday => 1, :name => "Restauração da Independência", :regions => [:pt]},
+            {:mday => 8, :name => "Imaculada Conceição", :regions => [:pt]},
             {:mday => 25, :name => "Natal", :regions => [:pt]}]
       }
     end
--- test/defs/test_defs_pt.rb
+++ test/defs/test_defs_pt.rb
@@ -14,10 +14,15 @@ class PtDefinitionTests < Test::Unit::TestCase  # :nodoc:
  Date.civil(2008,2,5) => 'Carnaval',
  Date.civil(2008,3,21) => 'Sexta-feira Santa',
  Date.civil(2008,3,23) => 'Páscoa',
+ Date.civil(2016,3,27) => 'Páscoa',
  Date.civil(2008,4,25) => 'Dia da Liberdade',
  Date.civil(2008,5,1) => 'Dia do Trabalhador',
+ Date.civil(2016,5,26) => 'Corpo de Deus',
  Date.civil(2008,6,10) => 'Dia de Portugal',
  Date.civil(2008,8,15) => 'Assunção de Nossa Senhora',
+ Date.civil(2008,10,5) => 'Dia da República',
+ Date.civil(2008,11,1) => 'Dia de Todos-os-Santos',
+ Date.civil(2008,12,1) => 'Restauração da Independência',
  Date.civil(2008,12,8) => 'Imaculada Conceição',
  Date.civil(2008,12,25) => 'Natal'}.each do |date, name|
   assert_equal name, (Holidays.on(date, :pt, :informal)[0] || {})[:name]
