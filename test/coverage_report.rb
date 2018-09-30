6f9f0e439ebdae1228326baa2ec704ab55fb3f5e
Phil Peble <ppeble@sittercity.com>
2018-09-29 23:07:35 -0500

    Remove simplecov-rcov gem and just use standard html reports

--- test/coverage_report.rb
+++ test/coverage_report.rb
@@ -1,8 +1,6 @@
 require 'simplecov'
-require 'simplecov-rcov'
 
 SimpleCov.minimum_coverage 99
-SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.coverage_dir 'reports/coverage'
 SimpleCov.start do
   add_filter 'lib/generated_definitions/'
