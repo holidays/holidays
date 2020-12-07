d8b13ac2150d69d5cfd42aef75b51d69465c66f8
Caleb Leung <caleb.leungg@gmail.com>
2020-12-07 15:39:02 +1000

    remove SA from observed xmas to additional (2021)

--- lib/generated_definitions/au.rb
+++ lib/generated_definitions/au.rb
@@ -55,8 +55,7 @@ module Holidays
             {:mday => 26, :name => "Boxing Day", :regions => [:au_act, :au_nsw, :au_qld, :au_vic, :au_wa]},
             {:mday => 26, :function => "additional_holiday_if_on_weekend(date)", :function_arguments => [:date], :name => "Additional public holiday Boxing Day", :regions => [:au_act, :au_nsw, :au_qld, :au_vic, :au_wa]},
             {:mday => 26, :observed => "to_tuesday_if_sunday_or_monday_if_saturday(date)", :observed_arguments => [:date], :name => "Boxing Day", :regions => [:au_tas, :au_nt, :au_sa]},
-            {:function => "to_weekday_if_boxing_weekend_from_year_or_to_tuesday_if_monday(year)", :function_arguments => [:year], :name => "Proclamation Day", :regions => [:au_sa]},
-            {:mday => 25, :observed => "to_monday_if_weekend(date)", :observed_arguments => [:date], :name => "Christmas Day", :regions => [:au_sa]}]
+            {:function => "to_weekday_if_boxing_weekend_from_year_or_to_tuesday_if_monday(year)", :function_arguments => [:year], :name => "Proclamation Day", :regions => [:au_sa]}]
       }
     end
 
