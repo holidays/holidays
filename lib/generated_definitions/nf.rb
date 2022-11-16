e06dd53a33028f49c98c932a8ae5d1ef26e10f25
Ben Aitcheson <benaitcheson@hotmail.com>
2022-11-16 22:49:41 +1000

    FIX: ran rake generate

--- lib/generated_definitions/nf.rb
+++ lib/generated_definitions/nf.rb
@@ -15,15 +15,18 @@ module Holidays
               0 => [{:function => "easter(year)", :function_arguments => [:year], :function_modifier => -2, :name => "Good Friday", :regions => [:nf]},
             {:function => "easter(year)", :function_arguments => [:year], :function_modifier => 1, :name => "Easter Monday", :regions => [:nf]}],
       1 => [{:mday => 1, :name => "New Year's Day", :regions => [:nf]},
+            {:mday => 1, :observed => "to_monday_if_weekend(date)", :observed_arguments => [:date], :name => "(additional day New Year's Day)", :regions => [:nf]},
             {:mday => 26, :observed => "to_monday_if_weekend(date)", :observed_arguments => [:date], :name => "Australia Day", :regions => [:nf]}],
-      3 => [{:mday => 6, :name => "Foundation Day", :regions => [:nf]}],
+      3 => [{:mday => 6, :name => "Foundation Day", :regions => [:nf]},
+            {:mday => 6, :observed => "to_monday_if_weekend(date)", :observed_arguments => [:date], :name => "(additional day Foundation Day)", :regions => [:nf]}],
       4 => [{:mday => 25, :name => "Anzac Day", :regions => [:nf]}],
       6 => [{:mday => 8, :observed => "to_monday_if_weekend(date)", :observed_arguments => [:date], :name => "Bounty Day", :regions => [:nf]},
             {:function => "monday_after_second_saturday(year)", :function_arguments => [:year], :name => "Queen's Birthday", :regions => [:nf]}],
       10 => [{:wday => 1, :week => 2, :name => "Norfolk Island Agricultural Show", :regions => [:nf]}],
-      11 => [{:wday => 3, :week => 4, :name => "Thanksgiving Day", :regions => [:nf]}],
+      11 => [{:wday => 3, :week => 5, :name => "Thanksgiving Day", :regions => [:nf]}],
       12 => [{:mday => 25, :name => "Christmas Day", :regions => [:nf]},
-            {:mday => 26, :observed => "to_monday_if_weekend(date)", :observed_arguments => [:date], :name => "Boxing Day", :regions => [:nf]}]
+            {:mday => 26, :name => "Boxing Day", :regions => [:nf]},
+            {:mday => 26, :observed => "to_monday_if_weekend(date)", :observed_arguments => [:date], :name => "(additional day Boxing Day)", :regions => [:nf]}]
       }
     end
 
